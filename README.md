b26cc7378bd1edfdbe685311053ac768fa02ada9
cronii <cronii@protonmail.com>
2023-06-18 05:52:19 -0400

    clean up

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ add text files to `/input` folder and run `node index.js`
 
 ## todo
 - finish single card translation mechanism
+- resolve words with no translations found
 - finish card creator function
 - implement batching
+- implement multifile input
 
 ## reach
 - OCR support
--- index.js
+++ index.js
@@ -7,23 +7,19 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
 const INPUT_PATH = './input/input.txt';
-const OUTPUT_PATH = './output/output.txt';
-const CONDENSED_OUTPUT = './output/test-output.json';
-const MASTER_LIST = '';
 
 const WAIT_TIME = 1000;
-const BATCH_SIZE = 100;
 
 const jisho = new JishoAPI();
 
 // Filter out particles, symbols, auxiliary verbs
 const FILTERED_WORD_TYPES = ['助詞', '記号', '助動詞'];
-const TYPE_DICTIONARY = {
-  副詞: 'Adverb',
-  名詞: 'Noun',
-  動詞: 'Verb',
-  形容詞: 'Adjective'
-};
+// const TYPE_DICTIONARY = {
+//   副詞: 'Adverb',
+//   名詞: 'Noun',
+//   動詞: 'Verb',
+//   形容詞: 'Adjective'
+// };
 
 // Wrapper for builing kuromoji tokenizer
 function buildTokenizer() {
@@ -61,15 +57,15 @@ async function translateText(text) {
   }
 }
 
-// Create anki token card
-function createTokenCard(token) {
-  return `test ${token}`;
-}
+// // Create anki token card
+// function createTokenCard(token) {
+//   return `test ${token}`;
+// }
 
-// Create anki line card
-function createLineCard(line) {
-  return `test ${line}`;
-}
+// // Create anki line card
+// function createLineCard(line) {
+//   return `test ${line}`;
+// }
 
 (async () => {
   try {
@@ -83,6 +79,7 @@ function createLineCard(line) {
     // await db.run('CREATE TABLE IF NOT EXISTS tokens (token TEXT PRIMARY KEY, type TEXT)');
     // await db.run('CREATE TABLE IF NOT EXISTS token_examples (token TEXT PRIMARY KEY, example_jp TEXT, example_en TEXT)');
     await db.run('CREATE TABLE IF NOT EXISTS jisho_translations (token TEXT PRIMARY KEY, result TEXT, mulitple_results BOOLEAN, direct_match BOOLEAN)');
+    await db.run('CREATE TABLE IF NOT EXISTS manual_resolution (token TEXT PRIMARY KEY, no_translation BOOLEAN)');
 
     const fileContents = await fs.promises.readFile(INPUT_PATH, 'utf8');
     const lines = fileContents.split('\n').filter(line => line !== '');
@@ -97,11 +94,11 @@ function createLineCard(line) {
       const line = lines[i];
       const translateLine = translatedLines[i];
 
-      // Parse contents for token cards to make
+      // parse line for usable token
       const tokens = tokenizer.tokenize(line);
 
       for (const token of tokens) {
-        if (!FILTERED_WORD_TYPES.includes(token.pos) && !uniqueTokenSet.has(token.basic_form)) {
+        if (!FILTERED_WORD_TYPES.includes(token.pos) && !uniqueTokenSet.has(token.basic_form) && token.basic_form !== '*') {
           const card = {
             ...token,
             example_jp: line,
@@ -113,9 +110,6 @@ function createLineCard(line) {
       }
     }
 
-    // check if cards exist in database
-
-    // Generate the placeholders for the parameterized query
     const uniqueTokens = Array.from(uniqueTokenSet);
     const placeholders = uniqueTokens.map((token) => `"${token}"`).join(',');
 
@@ -125,13 +119,6 @@ function createLineCard(line) {
     const existingTokens = new Set(rows.map(row => row.token));
     const newTokens = uniqueTokens.filter(value => !existingTokens.has(value));
 
-    // If new values are found, insert them into the database
-    // if (newTokens.length > 0) {
-    //   const insertQuery = `INSERT INTO tokens (token, ) VALUES ${newTokens.map(() => '(?)').join(',')}`;
-    //   await db.run(insertQuery, newTokens);
-    // }
-
-    // if not, use jisho translate
     for (const newToken of newTokens) {
       const jishoResults = await searchForPhraseAsync(newToken);
 
@@ -144,14 +131,12 @@ function createLineCard(line) {
         await db.run(insertQuery, [newToken, result, moreResults, directMatch]);
         console.log(`New Jisho entry for : ${newToken}`);
       } else {
+        const insertQuery = 'INSERT OR IGNORE INTO manual_resolution (token, no_translation) VALUES (?, ?)'
+        await db.run(insertQuery, [newToken, true]);
         console.log(`No Jisho results for: ${newToken}`);
       }
     }
 
-    // const translatedTokens = await translateTokens(filteredTokens);
-    // const formattedContent = JSON.stringify(translatedTokens, null, 2);
-    // // Write token cards
-    // await fs.promises.writeFile(CONDENSED_OUTPUT, formattedContent, 'utf8');
     console.log(`${cardsToCreate.length} cards created`);
   } catch (err) {
     console.error('Error:', err);
